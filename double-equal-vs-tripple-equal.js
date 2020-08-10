9e1c32fe39a5747ea7a03f4f8a0554c4afbe1940
redoy1102 <hridoyvlog9@gmail.com>
2020-08-11 02:27:36 +0600

    Double equal vs tripple equal concept cleared

---
+++ double-equal-vs-tripple-equal.js
@@ -0,0 +1,19 @@
+const num = 1 ;
+const num1 = true ;
+if(num === num1){
+    console.log('Condition is true');
+}
+else{
+    console.log('Condition is not true');
+}
+//print Condition is not true
+
+const num = 1 ;
+const num1 = true ;
+if(num == num1){
+    console.log('Condition is true');
+}
+else{
+    console.log('Condition is not true');
+}
+//print Condition is true
