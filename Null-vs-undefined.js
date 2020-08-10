29e8295ea27d9416c25427aa963aed1b8d8def10
redoy1102 <hridoyvlog9@gmail.com>
2020-08-11 02:05:39 +0600

    Finished Null vs undefinied for interview

---
+++ Null-vs-undefined.js
@@ -0,0 +1,19 @@
+//undefined
+// let pakhi;
+// console.log(pakhi);
+
+// function add(num1,num2){
+//     console.log(num1 + num2);
+//     return
+// }
+// const result = add(5,70);
+// console.log(result);
+
+// function add(num1,num2){
+//     console.log(num1,num2)
+// }
+// const result = add(13);
+// console.log(result);
+
+// const SmartDude ={Name: "Hridoy", Id: 20103046};
+// console.log(SmartDude.school);
