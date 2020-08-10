76a7c9d195b2ba3c7289433247395d3d0a40c67b
redoy1102 <hridoyvlog9@gmail.com>
2020-08-11 01:12:48 +0600

    I am advanced now.No more backpack

---
+++ true-false.js
@@ -0,0 +1,17 @@
+// falsy
+// 0
+//null
+//undefined
+//NaN
+
+//Truthy
+//'0', ' ', 6 ,[], {}; { }, [ ], 
+
+
+let name =[] 
+if(name){
+    console.log('Condition is true');
+}
+else{
+    console.log('Condition is False');
+}
